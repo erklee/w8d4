34d90bf9682d91167494b08fa97e9b3721cb7242
Eric Lee <erlee92@yahoo.com>
2023-12-07 14:17:07 -0500

    first commit

---
+++ argumentExercises.js
@@ -0,0 +1,47 @@
+function sum () {
+    let total = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        total += arguments[i]
+    }
+
+    return total; 
+};
+
+function sum1 (...args) {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i]
+    }
+}
+
+Function.prototype.myNewBind = function(context) {
+    let func = this;
+    let bArgs = Array.from(args).slice(1);
+    return function _bFunc() {
+        const cArgs = Array.from(args);
+        return func.apply(context, bArgs.concat(cArgs));
+    };
+};
+
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    says(sound, person) {
+      console.log(`${this.name} says ${sound} to ${person}!`);
+      return true;
+    }
+  }
+  
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const markov = new Cat("Markov");
+  const pavlov = new Dog("Pavlov");
+  
+//   markov.says("meow", "Ned");
+markov.says.myNewBind(pavlov, "meow", "Kush")();
